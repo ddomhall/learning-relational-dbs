96c9f3bb05b94f8fd53ef37e9fe01888e4a95667
ddomhall <domjshall@gmail.com>
2024-04-07 07:14:35 +0100

    13.3

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, QueryTypes } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 const main = async () => {
   try {
     await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
+    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    console.log(blogs)
     sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the database:', error)
