929dbd8bdc98bf16f9130f18331b36616df26156
ddomhall <domjshall@gmail.com>
2024-04-14 06:53:19 +0100

    13.19

---
+++ migrations/20240414_00_add_reading_list.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('reading_list', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+      },
+      blog_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'blogs', key: 'id' },
+      },
+    })
+    await queryInterface.addColumn('blogs', 'read', {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    })
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('reading_list')
+    await queryInterface.dropTable('blogs')
+  },
+}
--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
 const Blog = require('./blog')
 const User = require('./user')
+const UserNotes = require('./reading_list')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+User.belongsToMany(Blog, { through: UserNotes, as: 'marked_blogs' })
+Blog.belongsToMany(User, { through: UserNotes, as: 'users_marked' })
 
 module.exports = {
   Blog, User
