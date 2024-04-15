ad8601614713d872d63991c132243f0b149c5d3e
ddomhall <domjshall@gmail.com>
2024-04-15 05:17:33 +0100

    13.22

--- controllers/reading_lists.js
+++ controllers/reading_lists.js
@@ -7,4 +7,19 @@ router.post('/', tokenExtractor, async (req, res) => {
   res.json(addition)
 })
 
+router.put('/:id', tokenExtractor, async (req, res) => {
+  if (!req.body.read) {
+    res.status(400).json({error: 'needs read status'})
+  }
+
+  const item = await ReadingLists.findByPk(req.params.id)
+  if (item.userId !== req.decodedToken.id) {
+    res.status(401).json({error: 'not your list'})
+  }
+
+  item.read = req.body.read
+  await item.save()
+  res.json(item)
+})
+
 module.exports = router
